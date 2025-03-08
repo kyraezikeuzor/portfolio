1c505c7a25bdaec210fc430a389991228c5888d4
Kyra Ezikeuzor <kyraezikeuzor@gmail.com>
2025-03-08 12:00:22 -0600

    Metadata alterations - testing

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,16 +10,27 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Kyra Ezikeuzor",
   description: "Student, developer, and writer aspiring to study molecular biology, computer science, cognitive science, and creative writing, aiming for a career in AI medicine research, global health policy, and writing. ",
+  metadataBase: new URL('https://www.kyraezikeuzor.com'),
   openGraph: {
+    url: 'https://www.kyraezikeuzor.com',
+    type: "website",
+    title: "Kyra Ezikeuzor",
+    description: "Student, developer, and writer aspiring to study molecular biology, computer science, cognitive science, and creative writing, aiming for a career in AI medicine research, global health policy, and writing. ",
     images: [
       {
-        url: "/thumbnail.png",
+        url: "https://www.kyraezikeuzor.com/thumbnail.png",
         width: 1200,
         height: 630,
         alt: "Pink, red, and orange gradient art",
       },
     ],
-}
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Kyra Ezikeuzor",
+    description: "Student, developer, and writer aspiring to study molecular biology, computer science, cognitive science, and creative writing, aiming for a career in AI medicine research, global health policy, and writing. ",
+    images: ["https://www.kyraezikeuzor.com/thumbnail.png"],
+  }
 };
 
 export default function RootLayout({
