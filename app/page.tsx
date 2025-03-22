0f6533617758d9e15eeb2e5be3dc9d5f45c48a43
Kyra Ezikeuzor <kyraezikeuzor@gmail.com>
2025-03-22 15:20:59 -0500

    Synced socials list from database to site + modifed schema + styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import { Portfolio } from '@/lib/notion'
-import { About, Headline, Work, Projects, Writing, Press } from '@/components/ui/portfolio'
-import { SocialBanner } from '@/components/ui/socials'
+import { About, Socials, Headline, Work, Projects, Writing, Press } from '@/components/ui/portfolio'
 import type { Metadata } from "next";
 import { thumbnailUrl, selfieUrl } from '@/lib/constants';
 
@@ -28,12 +27,12 @@ export default async function Home() {
     <section className="flex flex-col justify-center gap-8">
       <header className='flex flex-col sm:flex-row items-start sm:items-center gap-5'>
         <img className='w-32 h-fit sm:w-32 sm:h-32 rounded-full' src={selfieUrl} alt='Picture of me'/>
-        <div className='flex flex-col items-start gap-3'>
+        <div className='flex flex-col items-start gap-2'>
           <div className='flex flex-col items-start gap-1'>
             <h1 className='text-4xl font-semibold tracking-tighter text-neutral-600 dark:text-neutral-300'>Kyra Ezikeuzor</h1>
             <Headline headline={portfolio.headline}/>
           </div>
-          <SocialBanner/>
+          <Socials socials={portfolio.socials}/>
         </div>
       </header>
       <About about={portfolio.about}/>
