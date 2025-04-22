c57f122f5ed32c74c12f19ef2e4b12a17979fd52
Kyra Ezikeuzor <kyraezikeuzor@gmail.com>
2025-04-21 22:09:46 -0500

    Reset navigate.tsx

--- components/ui/navigate.tsx
+++ components/ui/navigate.tsx
@@ -22,7 +22,7 @@ export function Navigate({ targetId, navigateId, parentPage, children }: Navigat
 
     window.scrollTo({
       top: targetPosition,
-      behavior: 'smooth',
+      behavior: 'instant',
     });
   }, []);
 
