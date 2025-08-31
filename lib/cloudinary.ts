7ae1ad9b54346f629a4d9e10ae25896a164897f6
Kyra Ezikeuzor <kyraezikeuzor@gmail.com>
2025-08-31 11:47:09 -0500

    Clean up

--- lib/cloudinary.ts
+++ lib/cloudinary.ts
@@ -3,7 +3,6 @@ import { v2 as cloudinary } from 'cloudinary';
 export async function config({
   cloudinaryUrl,
 }: {
-  // cloudinary://API_KEY:API_SECRET@CLOUD_NAME
   cloudinaryUrl: string;
 }) {
   const urlRegex = /^cloudinary:\/\/([a-z0-9-_]+):([a-z0-9-_]+)@([a-z0-9-_]+)$/i;
